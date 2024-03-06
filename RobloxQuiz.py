25c130f1653823d6d448c49463ac7aa59edf5bef
FanisPrograms <162288316+FanisPrograms@users.noreply.github.com>
2024-03-06 13:27:37 +0200

    Add files via upload

---
+++ RobloxQuiz.py
@@ -0,0 +1,123 @@
+coins = 0
+score = 0
+wrong = 0
+print ("Welcome to the Roblox Quiz made by FanisPrograms on GitHub")
+print ("Let's get started!")
+print ("There are 3 questions")
+print ("Difficulty = Easy")
+quest1 = input ("Question #1 = When did Roblox get created? ")
+if quest1 == 2006 :
+    print ("Correct!")
+    score += 1
+    shopquestion = input ("Whould you like to go to the shop? (Type y to go to the shop and n to continue)")
+    if shopquestion.lower() == ("y"):
+        print ("Welcome to the shop!")
+        print ("At this time we only have +1 score for 30 coins! Type 1 to buy the +1 score! ")
+        buy = input = ("What do you wanna buy")
+        if buy == 1 :
+            coins -= 30
+            score += 1
+            print ("You have successfully bought +1 score for 30 coins")
+        
+else:
+    print ("Wrong")
+    wrong += 1
+    shopquestion = input ("Whould you like to go to the shop? (Type y to go to the shop and n to continue)")
+    if shopquestion.lower() == ("y"):
+        print ("Welcome to the shop!")
+        print ("At this time we only have +1 score for 30 coins! Type 1 to buy the +1 score! ")
+        buy = input = ("What do you wanna buy")
+        if buy == 1 :
+            coins -= 30
+            score += 1
+            print ("You have successfully bought +1 score for 30 coins")
+        
+
+quest2 = input ("When did Arsenal release? ")
+if quest2 == 2016 :
+    print ("Correct!")
+    score += 1
+    shopquestion = input ("Whould you like to go to the shop? (Type y to go to the shop and n to continue)")
+    if shopquestion.lower() == ("y"):
+        print ("Welcome to the shop!")
+        print ("At this time we only have +1 score for 30 coins! Type 1 to buy the +1 score! ")
+        buy = input = ("What do you wanna buy")
+        if buy == 1 :
+            coins -= 30
+            score += 1
+            print ("You have successfully bought +1 score for 30 coins")
+        
+else:
+    print ("Wrong")
+    wrong += 1
+    shopquestion = input ("Whould you like to go to the shop? (Type y to go to the shop and n to continue)")
+    if shopquestion.lower() == ("y"):
+        print ("Welcome to the shop!")
+        print ("At this time we only have +1 score for 30 coins! Type 1 to buy the +1 score! ")
+        buy = input = ("What do you wanna buy")
+        if buy == 1 :
+            coins -= 30
+            score += 1
+            print ("You have successfully bought +1 score for 30 coins")
+        
+
+quest3 = input ("When did Pet Sim X release? ")
+if quest3 == 2021 :
+    print ("Correct!")
+    score += 1
+    shopquestion = input ("Whould you like to go to the shop? (Type y to go to the shop and n to continue)")
+    if shopquestion.lower() == ("y"):
+        print ("Welcome to the shop!")
+        print ("At this time we only have +1 score for 30 coins! Type 1 to buy the +1 score! ")
+        buy = input = ("What do you wanna buy")
+        if buy == 1 :
+            coins -= 30
+            score += 1
+            print ("You have successfully bought +1 score for 30 coins")
+        
+else:
+    print ("Wrong")
+    wrong += 1
+    shopquestion = input ("Whould you like to go to the shop? (Type y to go to the shop and n to continue)")
+    if shopquestion.lower() == ("y"):
+        print ("Welcome to the shop!")
+        print ("At this time we only have +1 score for 30 coins! Type 1 to buy the +1 score! ")
+        buy = input = ("What do you wanna buy")
+        if buy == 1 :
+            coins -= 30
+            score += 1
+            print ("You have successfully bought +1 score for 30 coins")
+        
+
+quest4 = input ("When did the Metaverse Champions event release? ")
+if quest4 == 2021 :
+    print ("Correct!")
+    score += 1
+    shopquestion = input ("Whould you like to go to the shop? (Type y to go to the shop and n to continue)")
+    if shopquestion.lower() == ("y"):
+        print ("Welcome to the shop!")
+        print ("At this time we only have +1 score for 30 coins! Type 1 to buy the +1 score! ")
+        buy = input = ("What do you wanna buy")
+        if buy == 1 :
+            coins -= 30
+            score += 1
+            print ("You have successfully bought +1 score for 30 coins")
+        
+else:
+    print ("Wrong")
+    wrong += 1
+    shopquestion = input ("Whould you like to go to the shop? (Type y to go to the shop and n to continue)")
+    if shopquestion.lower() == ("y"):
+        print ("Welcome to the shop!")
+        print ("At this time we only have +1 score for 30 coins! Type 1 to buy the +1 score! ")
+        buy = input = ("What do you wanna buy")
+        if buy == 1 :
+            coins -= 30
+            score += 1
+            print ("You have successfully bought +1 score for 30 coins")
+        
+
+print ("Your score is", score)
+print ("Your wrong answers are", wrong)
+
+
